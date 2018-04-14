8683af9836d56a55b46dd0dd624233f67d2418b9
Hugo <hugo.poupard@gmail.com>
2018-04-14 15:17:42 +0200

    added window.cpp

---
+++ src/window.cpp
@@ -0,0 +1,35 @@
+#include "headers/window.h"
+
+
+Window()
+{
+    screenX = 0;
+    screenY = 0;
+    winMutex = NULL;
+}
+
+~Window()
+{
+    winMutex = NULL;
+}
+
+bool createWindow()
+{
+
+}
+
+bool destroyWindow()
+{
+
+}
+
+void updateWindow()
+{
+
+}
+
+Image* getScreen()
+{
+
+}
+
