f950b229d579ee3755d4350822ae5d35cabcb5f8
Unknown <rfumeron@enseirb-matmeca.fr>
2018-04-16 18:05:37 +0200

    Finish test, OK !

--- src/palier0/testVectorimage.cpp
+++ src/palier0/testVectorimage.cpp
@@ -1,9 +1,18 @@
 #include "vector_image.h"
 
+#include <cv.hpp>
+
 int main(void)
 {
     VectorImage vi = VectorImage();
     vi.initVector();
-    vi.loadImage("textures/test0.png");
+    vi.loadImage("bin/textures/test0.png");
+    (vi.getImageFromIndex(0))->print();
+
+    namedWindow("Tamere", WINDOW_AUTOSIZE);
+    imshow("Tamere", *(vi.getImageFromIndex(0)->getMat()));
+
+    waitKey(0);
+
     return 0;
 }
